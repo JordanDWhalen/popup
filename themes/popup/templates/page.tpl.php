a5c55783cbeebd2fa5e4e8c0d24aa0bf7641bc35
Jordan Whalen <JordanWhalen@me.com>
2015-06-24 22:51:00 -0400

    Pushing up base theme.

--- themes/popup/templates/page.tpl.php
+++ themes/popup/templates/page.tpl.php
@@ -1,1 +1,9 @@
-<?php print render($page['content']); ?>
+<section class="content wrapper">
+	<header>
+		<?php print render($page['header']); ?>
+		<h1><?php print $site_name;?></h1>
+	</header>
+
+	<?php print render($page['content']); ?>
+
+</section>
