e79c693fedcd356bc97ea168753e5c6b81d3dcb1
Jordan Whalen <JordanWhalen@me.com>
2015-06-26 17:33:41 -0400

    Wrapping up remaining styling bugfixes.

---
+++ sites/all/themes/popup/template.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Implements hook_page_alter().
+ */
+function THEMENAME_page_alter($page) {
+  // Add meta tag for viewport, for easier responsive theme design.
+  $viewport = array(
+    '#type' => 'html_tag',
+    '#tag' => 'meta',
+    '#attributes' => array(
+      'name' => 'viewport',
+      'content' => 'width=device-width, initial-scale=1',
+    ),
+  );
+  drupal_add_html_head($viewport, ‘viewport’);
+}
+?>
